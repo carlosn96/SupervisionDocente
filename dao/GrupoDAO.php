c5009b18e8ab3157b3a3d1d633379011dff14b95
UNE <UNE@LAP-HP-UNE2>
2025-02-06 15:40:31 -0600

    Se agrega el atributo "grado" para Grupo

--- dao/GrupoDAO.php
+++ dao/GrupoDAO.php
@@ -3,19 +3,20 @@
 class GrupoDAO extends DAO {
 
     private const NOMBRE_TABLA = "grupo";
-    private const INSERTAR = "INSERT INTO " . self::NOMBRE_TABLA . " (clave, seudonimo, id_carrera, id_plantel) VALUES (?, ?, ?, ?)";
+    private const INSERTAR = "INSERT INTO " . self::NOMBRE_TABLA . " (clave, id_carrera, id_plantel, grado_actual) VALUES (?, ?, ?, ?)";
     private const LISTAR = "SELECT * FROM " . self::NOMBRE_TABLA;
     private const ACTUALIZAR = "CALL actualizar_grupo(?) ";
-    
+    private const RECUPERAR_GRADOS = "SELECT * FROM grupo_grado";
+
     public function agregar(Grupo $grupo) {
         $pre = new PreparedStatmentArgs();
         $pre->add("s", $grupo->getClave());
-        $pre->add("s", $grupo->getSeudonimo());
+        $pre->add("s", $grupo->getGrado());
         $pre->add("i", $grupo->getCarrera());
         $pre->add("i", $grupo->getPlantel());
         return $this->ejecutar_instruccion_preparada(self::INSERTAR, $pre);
     }
-    
+
     public function actualizar(Grupo $grupo) {
         $pre = new PreparedStatmentArgs();
         $pre->add("s", json_encode($grupo->to_array()));
@@ -25,12 +26,12 @@ class GrupoDAO extends DAO {
     private function buscar_grupo($where) {
         return $this->ejecutar_instruccion(self::LISTAR . "  " . $where)->fetch_all(MYSQLI_ASSOC);
     }
-    
+
     public function listar_grupos($carrera, $plantel) {
         $where = " WHERE id_carrera = $carrera AND id_plantel = $plantel";
         return $this->buscar_grupo($where);
     }
-    
+
     public function recuperar_grupo($id) {
         return $this->buscar_grupo(" WHERE id_grupo = $id")[0];
     }
@@ -38,8 +39,13 @@ class GrupoDAO extends DAO {
     public function eliminar($id) {
         return $this->eliminar_por_id(self::NOMBRE_TABLA, "id_grupo", $id);
     }
-    
+
     public function recuperar_turnos() {
         return $this->get_anum_values(self::NOMBRE_TABLA, "turno");
+    }
+
+    public function recuperar_grados() {
+        $grados = $this->ejecutar_instruccion(self::RECUPERAR_GRADOS);
+        return $grados ? $grados->fetch_all(MYSQLI_ASSOC) : [];
     }
 }
