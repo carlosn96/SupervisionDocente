fe552918dfc7f871663a09b73f43682c48e47ae5
HP <HP@DESKTOP-A7ORG5U>
2025-02-26 23:21:20 -0600

    Se quita supevisiondocente del url de root

--- public/assets/js/util.js
+++ public/assets/js/util.js
@@ -439,5 +439,5 @@ function removerAccentos(str) {
 }
 
 function getRootUrl() {
-    return `${window.location.protocol}//${window.location.host}`;
+    return `${window.location.protocol}//${window.location.host}/`;
 }
