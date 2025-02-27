5ce4e91b4c902c8047b582fc8e3d0f16c75c32df
HP <HP@DESKTOP-A7ORG5U>
2025-02-26 23:17:26 -0600

    Integración con los buttons de datatables

--- public/coordinador/includes/head.php
+++ public/coordinador/includes/head.php
@@ -6,6 +6,7 @@
     <title>Supervisión Docente | Coordinador </title>
     <link rel="shortcut icon" type="image/png" href="../../../assets/images/logos/favicon.ico" />
     <link rel="stylesheet" href='../../../assets/css/styles.min.css' />
-    <link rel="stylesheet" href='https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.min.css' />
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.2/css/buttons.bootstrap5.min.css">
 </head>
 
