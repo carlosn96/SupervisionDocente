18231e50c8ba97355cb0d4069a331a8be70f2d9f
HP <HP@DESKTOP-A7ORG5U>
2025-02-26 23:17:37 -0600

    Integración con los buttons de datatables

--- public/coordinador/includes/script.php
+++ public/coordinador/includes/script.php
@@ -6,7 +6,16 @@
 <script src="../../../assets/libs/simplebar/dist/simplebar.js"></script>
 <script src="../../../assets/js/sweetalert.min.js"></script>
 <script src="../../../../controller/sesion.js"></script>
-<script src="../../../assets/libs/datatables/dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.min.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/2.2.2/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.bootstrap5.min.js" ></script>
+
+<!-- JS para botones específicos como Excel, PDF -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
 <script src="../../../assets/js/util.js"></script>
